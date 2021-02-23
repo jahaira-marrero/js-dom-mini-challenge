f7358357b5bb9f86496a001e3017ce0771b93217
Jahaira Marrero <jahaira.marrero@gmail.com>
2021-02-23 17:24:59 -0500

    working on it still...

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
+// const header = document.querySelector("#header")
+const heading = document.getElementById('header')
 console.log("Here's your header:", header)
 
 
 /***** Deliverable 2 *****/
-
+console.log (document.getElementById('header').style.color = "green");
 
 
 /***** Deliverable 3 *****/
-// console.log('This is what the traveler object looks like: ', traveler)
-
+console.log('This is what the traveler object looks like: ', traveler)
+const person = document.querySelector(`h2`)
+person.textContent= traveler.name
+const nickname = document.querySelector('em')
+nickname.textContent = traveler.nickname
+const pic = document.querySelector(`img`)
+pic.alt = traveler.name
+pic.src = traveler.photo
 
 /***** Deliverable 4 *****/
-
-
+// Working on it...
 
 /***** Deliverable 5 *****/
+// Working on it...
